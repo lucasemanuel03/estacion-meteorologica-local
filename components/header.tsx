f286146038ed1a1388b14a6ef456a601d8c66408
lucasemanuel03 <leblucas3@gmail.com>
2026-01-07 00:10:25 -0300

    add isotipo

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,10 @@ export default function Header() {
     <header className="w-full bg-background p-4 shadow-md sticky top-0 z-50">
         <div className="mb-2 flex flex-col items-center justify-center">
           <Link href="/">
-            <h1 className="text-xl sm:text-4xl font-bold tracking-tight pt-2">Estación Meteorológica Local</h1>
+            <div className="flex gap-2">
+              <img src="/EML-isotipo.svg" alt="Estación Meteorológica Local" className="h-12 w-12" />
+              <h1 className="text-xl sm:text-4xl font-bold tracking-tight pt-2">Estación Meteorológica Local</h1>
+            </div>
           </Link>
         </div>
     </header>
